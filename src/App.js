57b5ad051e62b779983ec638dd15c9858a0c843c
UnderbedMonster <77287904+SteklovMihail@users.noreply.github.com>
2021-05-27 19:52:48 +0300

    добавил стате

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
+import React from "react";
 import "./App.css";
 import Todoitem from "./Todoitem.js";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Todo list</h1>
-      <div className="add-item">
-        <input type="text" id="add-item-input" />
-        <input type="button" value="+" />
-        <div id="items-list">
-          <Todoitem text="Первое дело"></Todoitem>
-          <Todoitem text="Первое дело"></Todoitem>
-          <Todoitem text="dfihsdfihsdfuhsdfuhfusdhfuisdhfhsidf"></Todoitem>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "Привет мир!!!",
+      itemsList: ["Первое дело", "Второе дело"],
+    };
+  }
+
+  addItemBntClickHandler() {
+    const newValue = document.getElementById("add-item-input").value;
+    const newItemsList = [newValue, ...this.state.itemsList];
+
+    if (newValue.trim() !== "") {
+      this.setState({ itemsList: newItemsList });
+    } else {
+    }
+  }
+
+  render() {
+    const itemsList = this.state.itemsList;
+    const itemsListElement = itemsList.map((item, i) => (
+      <Todoitem key={i} text={item}></Todoitem>
+    ));
+
+    return (
+      <div className="App">
+        <h1>{this.state.title}</h1>
+        <div className="add-item">
+          <input type="text" id="add-item-input" />
+          <input
+            type="button"
+            value="+"
+            className="add-item-btn"
+            onClick={this.addItemBntClickHandler.bind(this)}
+          />
         </div>
+        <div id="items-list">{itemsListElement}</div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
